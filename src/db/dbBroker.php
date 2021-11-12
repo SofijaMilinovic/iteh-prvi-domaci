d1c29c4f947346d2b1256f4b46cb10fdbed310f7
Sofija Milinovic <milinovicsofija@gmail.com>
2021-11-12 12:37:51 +0100

    ciscenje koda

--- src/db/dbBroker.php
+++ src/db/dbBroker.php
@@ -7,12 +7,8 @@ $dbName = "iteh-prvi-domaci";
 
 $connection = new mysqli($host, $username, $password, $dbName);
 
-echo "connection err number: " . $connection->connect_errno;
-
 if ($connection->connect_errno) {
     exit("Neuspesna konekcija: greska > " . $connection->connect_error . ", err kod > " . $connection->connect_errno);
-} else {
-    echo "Uspesna konekcija";
 }
 
 ?>
