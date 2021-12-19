d093da114438d3dc55abee2592df74779f77e658
Sofija Milinovic <milinovissofija@gmail.com>
2021-12-19 11:37:54 +0100

    export baze

--- sql/baza.sql
+++ sql/baza.sql
@@ -1,6 +1,6 @@
 /*
-SQLyog Community v13.1.7 (64 bit)
-MySQL - 10.4.21-MariaDB : Database - iteh-prvi-domaci
+SQLyog Community v13.1.6 (64 bit)
+MySQL - 8.0.23 : Database - iteh-prvi-domaci
 *********************************************************************
 */
 
@@ -12,7 +12,7 @@ MySQL - 10.4.21-MariaDB : Database - iteh-prvi-domaci
 /*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
-CREATE DATABASE /*!32312 IF NOT EXISTS*/`iteh-prvi-domaci` /*!40100 DEFAULT CHARACTER SET utf8mb4 */;
+CREATE DATABASE /*!32312 IF NOT EXISTS*/`iteh-prvi-domaci` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
 
 USE `iteh-prvi-domaci`;
 
@@ -21,50 +21,64 @@ USE `iteh-prvi-domaci`;
 DROP TABLE IF EXISTS `klijent`;
 
 CREATE TABLE `klijent` (
-  `klijentId` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `jmbg` varchar(13) NOT NULL,
   `ime` varchar(255) NOT NULL,
   `prezime` varchar(255) NOT NULL,
   `datumRodjenja` date NOT NULL,
-  PRIMARY KEY (`klijentId`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+  PRIMARY KEY (`jmbg`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 /*Data for the table `klijent` */
+
+insert  into `klijent`(`jmbg`,`ime`,`prezime`,`datumRodjenja`) values 
+('1111111111111','Pera','Peric','2021-12-06'),
+('2222222222222','Mika','Mikic','2021-12-08');
 
 /*Table structure for table `psihoterapeut` */
 
 DROP TABLE IF EXISTS `psihoterapeut`;
 
 CREATE TABLE `psihoterapeut` (
-  `psihoterapeutId` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `psihoterapeutId` int unsigned NOT NULL AUTO_INCREMENT,
   `username` varchar(255) NOT NULL,
   `password` varchar(255) NOT NULL,
   `ime` varchar(255) NOT NULL,
   `prezime` varchar(255) NOT NULL,
   PRIMARY KEY (`psihoterapeutId`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB AUTO_INCREMENT=22 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 /*Data for the table `psihoterapeut` */
+
+insert  into `psihoterapeut`(`psihoterapeutId`,`username`,`password`,`ime`,`prezime`) values 
+(20,'pera@gmail.com','Pera123','Pera','Peric'),
+(21,'sofija@gmail.com','Sofija123','Sofija','Milinovic');
 
 /*Table structure for table `seansa` */
 
 DROP TABLE IF EXISTS `seansa`;
 
 CREATE TABLE `seansa` (
-  `seansaId` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `seansaId` int unsigned NOT NULL AUTO_INCREMENT,
   `datum` date NOT NULL,
   `vreme` varchar(255) NOT NULL,
-  `trajanjeMin` int(10) unsigned NOT NULL,
+  `trajanjeMin` int unsigned NOT NULL,
   `beleske` varchar(10000) NOT NULL,
-  `klijentId` int(10) unsigned NOT NULL,
-  `psihoterapeutId` int(10) unsigned NOT NULL,
+  `psihoterapeutId` int unsigned NOT NULL,
+  `klijentJmbg` varchar(13) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
   PRIMARY KEY (`seansaId`),
   KEY `seansa_psihoterapeut_fk` (`psihoterapeutId`),
-  KEY `seansa_klijent_fk` (`klijentId`),
-  CONSTRAINT `seansa_klijent_fk` FOREIGN KEY (`klijentId`) REFERENCES `klijent` (`klijentId`),
+  KEY `seansa_klijent` (`klijentJmbg`),
+  CONSTRAINT `seansa_klijent` FOREIGN KEY (`klijentJmbg`) REFERENCES `klijent` (`jmbg`),
   CONSTRAINT `seansa_psihoterapeut_fk` FOREIGN KEY (`psihoterapeutId`) REFERENCES `psihoterapeut` (`psihoterapeutId`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB AUTO_INCREMENT=19 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 /*Data for the table `seansa` */
+
+insert  into `seansa`(`seansaId`,`datum`,`vreme`,`trajanjeMin`,`beleske`,`psihoterapeutId`,`klijentJmbg`) values 
+(14,'2021-12-17','15:45',40,'Pozrtvovan',20,'1111111111111'),
+(16,'2021-12-19','20:30',30,'Teska depresija',21,'2222222222222'),
+(17,'2021-12-19','15:20',50,'Sreca sreca radost',21,'1111111111111'),
+(18,'2021-12-08','12:30',60,'Sve je bilo ok',20,'2222222222222');
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
 /*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
